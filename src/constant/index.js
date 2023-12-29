b60caed1cf8321246d0671e0172855c5643231a1
Kaushik Biswas <kbutsho@gmail.com>
2023-12-30 00:14:31 +0600

    update constant

--- src/constant/index.js
+++ src/constant/index.js
@@ -11,3 +11,36 @@ export const STATUS = {
     PENDING: 'pending',
     SHOW_ALL: 'show all'
 }
+export const TIME_SLOT = {
+    AM_06: '06 AM',
+    AM_07: '07 AM',
+    AM_08: '08 AM',
+    AM_09: '09 AM',
+    AM_10: '10 AM',
+    AM_11: '11 AM',
+    PM_12: '12 PM',
+    PM_01: '01 PM',
+    PM_02: '02 PM',
+    PM_03: '03 PM',
+    PM_04: '04 PM',
+    PM_05: '05 PM',
+    PM_06: '06 PM',
+    PM_07: '07 PM',
+    PM_08: '08 PM',
+    PM_09: '09 PM',
+    PM_10: '10 PM',
+    PM_11: '11 PM',
+    AM_12: '12 AM',
+    SHOW_ALL: 'show all'
+}
+
+export const DAY = {
+    SATURDAY: 'saturday',
+    SUNDAY: 'sunday',
+    MONDAY: 'monday',
+    TUESDAY: 'tuesday',
+    WEDNESDAY: 'wednesday',
+    THURSDAY: 'thursday',
+    FRIDAY: 'friday',
+    SHOW_ALL: 'show all'
+}
