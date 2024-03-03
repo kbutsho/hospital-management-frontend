af546d5d45dbdb962007530787337d1b1bd76baa
Kaushik Biswas <kbutsho@gmail.com>
2024-03-03 20:02:38 +0600

    added doctor

--- src/components/home/doctor/Doctor.js
+++ src/components/home/doctor/Doctor.js
@@ -1,9 +1,28 @@
-
+import { doctorData } from './doctorData';
+import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
 
 const Doctor = () => {
     return (
-        <div className='d-flex justify-content-center align-items-center' style={{ height: "100vh" }}>
-            <h3 className='fw-bold'>find doctor area</h3>
+        <div className="container py-5">
+            <h2 className="text-center mb-5 fw-bold text-uppercase text-success">Doctors</h2>
+            <div className="row">
+                {doctorData.map((doctor, index) => (
+                    <div key={index} className="col-md-3">
+                        <div className={`mb-4 ${styles.doctorCard}`}>
+                            <div className="card-body">
+                                <Image style={{ borderRadius: "6px 6px 0 0" }} height={80} width={80} layout='responsive' src={doctor.image} alt={doctor.name} />
+                                <div className='px-3 pb-3'>
+                                    <h5 style={{ minHeight: "50px" }} className="card-title fw-bold text-success mt-4  text-uppercase">{doctor.name}</h5>
+                                    <div className='mt-3' style={{ minHeight: "50px" }}>
+                                        <small className="card-text" >{doctor.designation}</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
