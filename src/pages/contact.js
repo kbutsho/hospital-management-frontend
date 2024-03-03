947b3bc422dc98a911ed076dd9a3ab4b041d7c49
Kaushik Biswas <kbutsho@gmail.com>
2024-03-03 20:03:14 +0600

    added contact us page

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import Contact from '@/components/home/contact/Contact';
+import MainLayout from '@/layouts/MainLayout';
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <Contact />
+        </div>
+    );
+};
+
+export default ContactPage;
+ContactPage.getLayout = function getLayout(page) {
+    return <MainLayout>{page}</MainLayout>;
+};
