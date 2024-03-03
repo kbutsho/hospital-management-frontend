fe4c01075ab6eb4aa50313b6247e855355b38763
Kaushik Biswas <kbutsho@gmail.com>
2024-03-03 20:02:52 +0600

    added demo doctor date

---
+++ src/components/home/doctor/doctorData.js
@@ -0,0 +1,10 @@
+export const doctorData = [
+    { name: "Prof. Dr. Deen Mohd. Noorul Huq", designation: "MBBS (Dhaka), DO (DU), MS (Oph)", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/02/fff-255x277.jpg" },
+    { name: "Prof. Sheikh Mohammed Hossain", designation: "MBBS, DO, MS, Fellow Glaucoma (USA) Glaucoma and Cataract", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/02/Sharfuddin.jpg" },
+    { name: "Prof. A.H.M Enayet Hussain", designation: "F.R.C.S (Glass.UK), F.C.P.S (BD)", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/02/Enayet.jpg" },
+    { name: "Prof. Dr. Md. Abid Kamal", designation: " MBBS, FCPS (Eye), MS (Eye) DU, Fellow Who(Oculoplasty) India", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/09/Prof.-Dr.-Md.-Abid-Kamal-MBBS-FCPS-Eye-MS-Eye-DU-Fellow-WhoOculoplasty-India-Phaco-Surgeon-Prof.-Enam-MCH..jpg" },
+    { name: "Prof. Md. Abdul Quader", designation: "MBBS, DO, FCPS & MS (Oph.), Cornea, LASIK & Phaco Surgeon", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/09/Prof.-Md.-Abdul-Quader-MBBS-DO-FCPS-MS-Oph.-Cornea-LASIK-Phaco-Surgeon.jpg" },
+    { name: "Dr. Shams Mohammed Noman", designation: "FCPS.DCO.FICO.MBBS Associate professor", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/03/Dr-Shams-Mohammed-Noman-255x277.jpg" },
+    { name: "Dr. Rajashree Das", designation: "FCPS (Eye) Fellowship Phaco, Cornea, LASIC, keratoplasty (India)", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/02/Dr.-Rajashree-Das-255x277.jpg" },
+    { name: "Prof. Mohammad Ghulam Rasul", designation: "MBBS, DO, FCPS Phaco, Medical Retina, Orbit & Oculoplasty", image: "https://deenmohdeyehospital.com/wp-content/uploads/2022/02/Ghulam.jpg" },
+]
