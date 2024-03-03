c5b690e5fe604e2920f9b6a1092be8d50f4cf135
Kaushik Biswas <kbutsho@gmail.com>
2024-03-03 20:02:01 +0600

    settings update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['deenmohdeyehospital.com'],
+  },
 }
 
 module.exports = nextConfig
