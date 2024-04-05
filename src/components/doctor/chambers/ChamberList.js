19996416a1e8b42a54e5153b38ae88ad43fc989f
Kaushik Biswas <kbutsho@gmail.com>
2024-04-05 11:10:55 +0600

    fix chamber list

--- src/components/doctor/chambers/ChamberList.js
+++ src/components/doctor/chambers/ChamberList.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "@/styles/administrator/List.module.css"
 import { config } from "@/config/index";
 import Cookies from 'js-cookie';
