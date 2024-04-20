b4a973603bf365ee15ebabe9fe11a511f4f55566
Kaushik Biswas <kbutsho@gmail.com>
2024-04-20 16:41:55 +0600

    update appointment list

--- src/components/administrator/appointment/AppointmentList.js
+++ src/components/administrator/appointment/AppointmentList.js
@@ -527,8 +527,8 @@ const AppointmentList = () => {
                                                             </td>
                                                             <td>
                                                                 <div className='d-flex justify-content-center table-btn'>
-                                                                    <button style={{ border: "0" }} className='btn btn-primary btn-sm mx-1'><AiFillEye className='mb-1' /></button>
-                                                                    <button style={{ border: "0" }} className='btn btn-success btn-sm mx-1'><AiFillEdit className='mb-1' /></button>
+                                                                    {/* <button style={{ border: "0" }} className='btn btn-primary btn-sm mx-1'><AiFillEye className='mb-1' /></button>
+                                                                    <button style={{ border: "0" }} className='btn btn-success btn-sm mx-1'><AiFillEdit className='mb-1' /></button> */}
                                                                     <button style={{ border: "0" }} onClick={() => toggleDeleteModal(data.id, data.name)} className='btn btn-danger btn-sm mx-1'><AiFillDelete className='mb-1' /></button>
                                                                 </div>
                                                             </td>
